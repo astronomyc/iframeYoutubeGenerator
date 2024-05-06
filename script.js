720d42e01a1fc064ce88dd59d2412202c744c29c
David <devastronomyc@outlook.com>
2024-05-06 11:11:04 -0500

    Initial Codes

---
+++ script.js
@@ -0,0 +1,18 @@
+// Script para generar el code embed de youtube personalizado
+// Generator and add iframe to page player after press button with muted and autoplay
+document.getElementById("genetator").addEventListener("click", function () {
+    let url = document.getElementById("url").value;
+    let video_id = url.split("v=")[1];
+    let ampersandPosition = video_id.indexOf("&");
+    if (ampersandPosition != -1) {
+        video_id = video_id.substring(0, ampersandPosition);
+    }
+    let embed = document.getElementById("embed");
+    embed.value = `<iframe
+width="100%"
+height="100%"
+src="https://www.youtube.com/embed/${video_id}?autoplay=1&mute=1&cc_load_policy=0">
+</iframe>`;
+    let iframeplayer = document.getElementById("iframeplayer");
+    iframeplayer.innerHTML = embed.value;
+});
